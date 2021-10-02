526d048d5f169a5cfb1dc7680e889fb311616280
Yonder Peña <djyonder@gmail.com>
2021-10-01 22:36:55 -0500

    funcionando el filter buscador de post, proyecto completo

--- script.js
+++ script.js
@@ -49,14 +49,33 @@ function showLoading() {
   }, 1000);
 }
 
+// Filter posts by input
+function filterPosts(e) {
+  const term = e.target.value.toUpperCase();
+  const posts = document.querySelectorAll(".post");
+
+  posts.forEach(post => {
+
+  // posts.forEach(post => {
+    const title = post.querySelector(".post-title").innerText.toUpperCase();
+    const body = post.querySelector(".post-body").innerText.toUpperCase();
+
+    if (title.indexOf(term) > -1 || body.indexOf(term) > -1) {
+      post.style.display = "flex";
+    } else {
+      post.style.display = "none";
+    }
+  });
+}
 //Show initial post
 showPost();
 
 window.addEventListener("scroll", () => {
-  const { scrollTop, scrollHeight, clientHeight } =
-  document.documentElement;
+  const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
 
   if (scrollTop + clientHeight >= scrollHeight - 5) {
     showLoading()
   }
 });
+
+filter.addEventListener("input", filterPosts);
